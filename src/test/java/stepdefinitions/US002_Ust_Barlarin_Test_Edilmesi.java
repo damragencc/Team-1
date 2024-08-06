affbc1786350d8de15c5ee06862597085c82f2bc
Esra Celik <esracelik4623@gmail.com>
2024-08-06 12:47:51 +0200

    US_002 degisiklik yapildi

--- src/test/java/stepdefinitions/US002_Ust_Barlarin_Test_Edilmesi.java
+++ src/test/java/stepdefinitions/US002_Ust_Barlarin_Test_Edilmesi.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
-public class US02 {
+public class US002_Ust_Barlarin_Test_Edilmesi {
 
     private WebDriver driver;
 
